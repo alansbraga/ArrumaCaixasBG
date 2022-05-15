81eaf39566a63b406612db6c1ae64ebbcbf8b3b9
Alan Braga <alansbraga@gmail.com>
2022-05-15 10:12:40 -0300

    Resultado temporário melhorado.

--- ArrumaCaixasBG.MostrarImagens/MostrarResultadoEmImagens.cs
+++ ArrumaCaixasBG.MostrarImagens/MostrarResultadoEmImagens.cs
@@ -5,19 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Xml.Linq;
 
 namespace ArrumaCaixasBG.MostrarImagens;
 
 internal class MostrarResultadoEmImagens : IMostrarResultado
 {
+    string pastaSalvar = @"c:\tmp\ArrumaBG\ImagemPrateleira";
+    Color[] cinzas = new Color[]
+    {
+        Color.Gray,
+        Color.DarkGray,
+        Color.DarkSlateGray,
+        Color.DimGray,
+        Color.LightGray,
+        Color.LightSlateGray,
+        Color.SlateGray
+    };
+
     public void Mostrar(ResultadoOrganizacao resultado)
     {
+        Directory.Delete(pastaSalvar, true);
+        Directory.CreateDirectory(pastaSalvar);
+        var cinzaAtual = 0;
         var lista = new List<CaixaCorPrateleira>();
         var rnd = new Random();
         foreach (var prateleira in resultado.Prateleiras)
         {
             using var imagem = new Bitmap((int)prateleira.Largura + 2, (int)prateleira.Altura + 2);
-            //using var imagemCinza = new Bitmap((int)prateleira.Largura + 2, (int)prateleira.Altura + 2);
+            using var imagemCinza = new Bitmap((int)prateleira.Largura + 2, (int)prateleira.Altura + 2);
             var yMax = (int)(imagem.Height - 1);
             var caixasAgrupadas = prateleira
                 .Caixas
@@ -29,7 +46,9 @@ internal class MostrarResultadoEmImagens : IMostrarResultado
                 foreach (var caixa in caixaAgrupada.OrderBy(c => c.X).ThenBy(c => c.Y))
                 {
                     var cor = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
-                    lista.Add(new CaixaCorPrateleira(prateleira.Nome, caixaAgrupada.Key, caixa.Nome, cor));
+                    var percVolume = (prateleira.VolumeNaoUtilizado / prateleira.Volume) * 100;
+                    percVolume = Math.Round(100 - percVolume, 2);
+                    lista.Add(new CaixaCorPrateleira(prateleira.Nome, caixaAgrupada.Key, caixa.Nome, cor, percVolume));
                     for (var x = (int)caixa.X; x < (int)(caixa.X + caixa.Largura); x++)
                     {
                         for (var y = (int)caixa.Y; y < (int)(caixa.Y + caixa.Altura); y++)
@@ -38,30 +57,35 @@ internal class MostrarResultadoEmImagens : IMostrarResultado
 
                             var yImagem = yMax - y;
                             imagem.SetPixel(x, yImagem, cor);
+                            imagemCinza.SetPixel(x, yImagem, cinzas[cinzaAtual]);
                         }
                     }
+                    cinzaAtual++;
+                    if (cinzaAtual >= cinzas.Count())
+                        cinzaAtual = 0;
                 }
-                imagem.Save($@"c:\tmp\ArrumaBG\ImagemPrateleira\{prateleira.Nome}-{caixaAgrupada.Key}.bmp");
-                /*
-                for (int x = 0; x < imagem.Width; x++)
-                {
-                    for (int y = 0; y < imagem.Height; y++)
-                    {
-                        var c = imagem.GetPixel(x, y);
-                        var novoC = Color.FromArgb((int)(c.R * 0.299), (int)(c.G * 0.587), (int)(c.B * 0.114));
-                        imagem.SetPixel(x, y, novoC);
-                    }
-                }
-                */
+                imagem.Save($@"{pastaSalvar}\{prateleira.Nome}-{caixaAgrupada.Key}.bmp");
+                CopiaImagem(imagem, imagemCinza);
             }
 
         }
-        CriaHtml(lista);
+        CriaHtml(resultado, lista);
 
 
     }
 
-    private void CriaHtml(List<CaixaCorPrateleira> lista)
+    private void CopiaImagem(Bitmap imagem, Bitmap imagemCinza)
+    {
+        for (int x = 0; x < imagemCinza.Width; x++)
+        {
+            for (int y = 0; y < imagem.Height; y++)
+            {
+                imagem.SetPixel(x, y, imagemCinza.GetPixel(x, y));
+            }
+        }
+    }
+
+    private void CriaHtml(ResultadoOrganizacao resultado, List<CaixaCorPrateleira> lista)
     {
         var sb = new StringBuilder(@"<!DOCTYPE html><html><head>
 <style>
@@ -85,21 +109,39 @@ internal class MostrarResultadoEmImagens : IMostrarResultado
 
         </head><body>");
 
-        var prateleiras = lista.GroupBy(l => l.Prateleira);
+
+        if (resultado.CaixasSobrando.Any())
+        {
+            sb.AppendLine("<h1>Caixas Sobrando</h1>");
+
+            sb.AppendLine("<ul>");
+            foreach (var caixa in resultado.CaixasSobrando)
+            {
+                sb.Append("<li>");
+                sb.AppendLine(caixa.Nome);
+            }
+            sb.AppendLine("</ul>");
+        }
+
+        var prateleiras = lista.GroupBy(l => new { l.Prateleira, l.PercentualUsado });
 
         foreach (var prateleira in prateleiras)
         {
-            sb.AppendLine($"<h1>{prateleira.Key}</h1>");
+            sb.AppendLine($"<h1>{prateleira.Key.Prateleira}</h1>");
+
+            var percVolume = prateleira.Key.PercentualUsado;
+            sb.AppendLine($"<p>Volume Utilizado: {percVolume} %</p>");
 
             var passos = prateleira.GroupBy(p => p.Posicao)
                 .OrderBy(p => p.Key);
-
+            var numPasso = 0;
             foreach (var passo in passos)
             {
-                sb.AppendLine($"<h3>{passo.Key}</h3>");
+                numPasso++;
+                sb.AppendLine($"<h3>{numPasso}</h3>");
                 sb.AppendLine("<div class=\"passo\">");
                 sb.AppendLine($"<div class=\"imagem\">");
-                sb.AppendLine($"<img src=\"{prateleira.Key}-{passo.Key}.bmp\" />");
+                sb.AppendLine($"<img src=\"{prateleira.Key.Prateleira}-{passo.Key}.bmp\" />");
                 sb.AppendLine("</div>");
                 sb.AppendLine($"<div class=\"lista\">");
                 //sb.AppendLine("<ul>");
@@ -117,7 +159,8 @@ internal class MostrarResultadoEmImagens : IMostrarResultado
         }
 
         sb.AppendLine("</body></html>");
-        File.WriteAllText($@"c:\tmp\ArrumaBG\ImagemPrateleira\arrumacao.html", sb.ToString());
+        File.WriteAllText($@"{pastaSalvar}\arrumacao.html", sb.ToString());
+        File.WriteAllText($@"{pastaSalvar}\arrumacao.json", JsonSerializer.Serialize(resultado));
     }
 
     private static string HexConverter(Color c)
@@ -126,4 +169,4 @@ internal class MostrarResultadoEmImagens : IMostrarResultado
     }
 }
 
-internal record CaixaCorPrateleira(string Prateleira, int Posicao, string Caixa, Color Cor);
+internal record CaixaCorPrateleira(string Prateleira, int Posicao, string Caixa, Color Cor, decimal PercentualUsado);
