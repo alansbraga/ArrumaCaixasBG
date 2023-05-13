6c1b847046fb2cf6586f74ab566f249047993ae1
Alan Braga <alansbraga@gmail.com>
2023-05-13 17:37:50 -0300

    Separando a organização em duas telas

---
+++ ArrumaCaixasBG.Blazor/Codigo/InicioOrganizacao.cs
@@ -0,0 +1,7 @@
+namespace ArrumaCaixasBG.Blazor.Codigo;
+
+public class InicioOrganizacao
+{
+    public byte[] Prateleiras { get; set; } = Array.Empty<byte>();
+    public byte[] Caixas { get; set; } = Array.Empty<byte>();
+}
